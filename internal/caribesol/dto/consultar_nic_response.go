58414cb15241c845b786656ba49b32ffc4cf3399
Mono <ferch5003@gmail.com>
2024-04-03 23:21:07 -0500

    enh: Add Historial data to CaribeSol DTO Response

--- internal/caribesol/dto/consultar_nic_response.go
+++ internal/caribesol/dto/consultar_nic_response.go
@@ -4,7 +4,23 @@ type Estado string
 
 const NoneEstado Estado = "0"
 
+type Historial struct {
+	ID               int    `json:"id"`
+	NuevaFecha       string `json:"nuevafecha"`
+	NIC              string `json:"nic"`
+	NoDocumento      string `json:"no_documento"`
+	NombreDocumento  string `json:"nombre_documento"`
+	Municipio        string `json:"municipio"`
+	Barrio           string `json:"barrio"`
+	Via              string `json:"via"`
+	Crucero          string `json:"crucero"`
+	Placa            string `json:"placa"`
+	NombreReclamante string `json:"nombre_reclamante"`
+	Estado           string `json:"estado"`
+}
+
 type ConsultarNICDTOResponse struct {
-	Estado Estado `json:"estado"`
-	Msg    string `json:"Msg"`
+	Estado    Estado      `json:"estado"`
+	Msg       string      `json:"msg"`
+	Historial []Historial `json:"historial"`
 }
